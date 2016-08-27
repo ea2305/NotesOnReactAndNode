616a60db576553b8747404712d56249433583186
Elihu <ea_2305@hotmail.com>
2016-08-26 21:07:55 -0500

    configur information to practice

---
+++ app/index.js
@@ -0,0 +1,13 @@
+//Librerias importantes
+import React from 'react';
+import { render } from 'react-dom';
+
+//Componentes y configuraciones
+import './app.global.css';
+import App from './App';
+
+
+render(
+  <App />,
+  document.getElementById('root')
+);
